d9d1654e3e54ccb74e85f509624b181ab10156ac
Богатов <siddha05.sb@gmail.com>
2024-10-11 19:23:56 +0300

    add signals

--- P2PChat.Server/Hubs/SignalHub.cs
+++ P2PChat.Server/Hubs/SignalHub.cs
@@ -7,32 +7,20 @@ namespace P2PChat.Server.Hubs
     public class SignalHub : Hub
     {
         ILogger<SignalHub> _logger;
-        static List<ChatUser> _users = new();
 
         public async Task NewUser(string username)
         {
-            if (_users.Count < 2)//Assume only 2 user
-            {
-                _users.Add(new ChatUser { Name = username, ConnectionID = Context.ConnectionId });
-                await Clients.Others.SendAsync("on_user_add", username);
-                _logger.LogInformation($"User connected {username} {Context.ConnectionId}. Total {_users.Count}");
-                if (_users.Count is 2 )
-                {
-                    await SendReadyToConnect(_users[0].ConnectionID);
-                }
-            }
-            else
-            {
-                _logger.LogInformation("Already full users list");
-            }
+            var user = new ChatUser { Name = username, ConnectionID = Context.ConnectionId };
+            await Groups.AddToGroupAsync(Context.ConnectionId, "1122");
+            await Clients.OthersInGroup("1122").SendAsync("on_user_add", JsonSerializer.Serialize(user));
+            _logger.LogInformation($"User connected {username} {Context.ConnectionId}");
         }
 
-        private async Task SendReadyToConnect(string target) => await Clients.User(target).SendAsync("on_ready");
 
         public async Task SendSignal(string signal, string target)
         {
-            _logger.LogWarning($"Got signal {signal}");
-            await Clients.User(target).SendAsync("on_signal" , signal);
+            _logger.LogWarning($"Got signal {signal} for {target}");
+            await Clients.Clients(target).SendAsync("on_signal", signal);
         }
 
         public SignalHub(ILogger<SignalHub> loger) => _logger = loger;
--- p2pchat.client/src/App.jsx
+++ p2pchat.client/src/App.jsx
@@ -9,8 +9,14 @@ function App() {
     const [text, setText] = useState('');
     const [isConnect, setisConnect] = useState(false);
     const [hub, setHub] = useState(null);
-    const [peerConnection, setPeerConnection] = useState();
+    //const [peerConnection, setPeerConnection] = useState();
     const [userName, setUserName] = useState('Apollo');
+    //const [otherUser, setOtherUser] = useState();
+    //const [dataChannel, setDataChannel] = useState();
+
+    var dataChannel;
+    var peerConnection;
+    var otherUser;
 
     const RTCconfig = {
         iceServers: [
@@ -19,49 +25,102 @@ function App() {
     };
 
     useEffect(() => {
-        setMessages(["Connecting to server..."]);
+        //setMessages(["Connecting to server..."]);
         const connection = new HubConnectionBuilder().withUrl('https://localhost:7036/signal').withAutomaticReconnect().build();
         connection.start().then(result => {
             setHub(connection);
             console.log('Connection established');
-            connection.on("on_user_add", msg => {
-                console.log("New user added", msg);
-            });
-            connection.on("on_signal", msg => {
-                console.log("Got Signal", msg);
-            });
-            connection.on("on_ready", msg => {
-                console.log("Ready to connect");
-            });
-        })
-            .catch(e => console.log('Connection failed: ', e));
-    }, []);
+            //connection.on("on_user_add", msg => {
+            //    setOtherUser(msg);
+            //    sendOffer();
+            //    //peerConnection.createOffer().then(desc => {
+            //    //    peerConnection.setLocalDescription(desc);
+            //    //    console.log("Local description", desc);
+            //    //    hub.invoke("SendSignal", desc, otherUser.ConnectionID);
+            //    //    console.log("Ready to connect");
+            //    //})
+            //});
+            //connection.on("on_signal", msg => {
+            //    console.log("Got Signal", msg);
+            //});
+            //connection.on("on_ready", () => {
+            //    console.log("Ready to connect");
+            //    peerConnection.createOffer().then(desc => {
+            //        peerConnection.setLocalDescription(desc);
+            //        console.log("Local description", desc);
+            //        hub.invoke("SendSignal", desc, otherUser.ConnectionID);
+            //        console.log("Ready to connect");
+            //    }
 
-    const send = () => {
-        setMessages([...messages, text]);
-        setText("");
+            //    );
+            //    //const offer = await peerConnection.createOffer();
+            //    //await peerConnection.setLocalDescription(offer);
+            //    //console.log("Created offer", offer);
+            //    //hub.invoke("SendSignal", offer, otherUser.ConnectionID);
+            //});
+        }).catch(e => console.log('Connection failed: ', e));
+    }, []);
+    
+    function send(){
+        //setMessages([...messages, text]);
+        //setText("");
+        
     }
-    const connect = () => {
-        hub.invoke("NewUser", userName);
+    function connect(){
+        hub.on("on_user_add", onAddUser);
+        hub.on("on_signal", onGotSignal);
+       
         let peerConn = new RTCPeerConnection(RTCconfig);
+        let data = peerConn.createDataChannel("chat");
         peerConn.onicecandidate = event => {
             if (event.candidate) {
-                hub.invoke("SendSignal", event.candidate);
+                console.log("Got candidate", event.candidate);
+                hub.invoke("SendSignal", JSON.stringify(event.candidate), otherUser.ConnectionID);
             }
         }
-        setPeerConnection(peerConn);
+        data.onopen = channelState;
+        data.onclose = channelState;
+        data.onmessage = msg => console.log(msg);
+        dataChannel =data;
+        peerConnection = peerConn;
+        hub.invoke("NewUser", userName).then(() => setMessages([...messages, 'Connected to signal server'])).catch(e => ShowError(e));
+        
+    }
+    function ShowError(e) {
+        console.log(e);
+    }
+    function onAddUser(user) {
+        console.log("New User", user);
+        var u = JSON.parse(user);
+        otherUser = u;
+        sendOffer(u);
+        
+    }
+    async function onGotSignal(signal) {
+        
+        var obj = JSON.parse(signal);
+        if (obj.type) {
+            if (obj.type === "offer") {
+                var answer = await peerConnection.createAnswer();
+                console.log("Create answer", answer);
+                peerConnection.setLocalDescription(answer);
+                hub.invoke("SendSignal", JSON.stringify(answer), user.ConnectionID);
+            }
+        }
     }
 
     const textChanged = (val) => {
         setText(val);
     }
-
-    const createOffer = async () => {
+    function channelState() {
+        console.log("Channel state is", dataChannel.readyState);
+    }
+    async function sendOffer(user) {
         const offer = await peerConnection.createOffer();
         await peerConnection.setLocalDescription(offer);
-        hub.invoke("S")
+        hub.invoke("SendSignal", JSON.stringify(offer), user.ConnectionID);
     }
-   
+    
     return (
         <div>
             <h1>P2P Chat</h1>
